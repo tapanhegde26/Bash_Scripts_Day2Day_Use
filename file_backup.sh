41fdbd7ce1910c8d72a4960cb9cbdb942c193057
Tapan Hegde <55941193+tapanhegde26@users.noreply.github.com>
2024-07-08 11:24:50 +0530

    file backup script committed

---
+++ file_backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+backup_dir = "/path/to/backup"
+source_dir = "/path/to/source"
+
+#Create time-stamped backup of source directory
+
+tar -czf "$backup_dir/backup_$(date +%Y%m%d_%H%M%S).tar.gz" "$source_dir"
